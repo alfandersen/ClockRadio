555424650caabf2ed9e2b6aea4a71657593eeab5
Alf <alf.andersen.1986@gmail.com>
2017-07-07 16:47:59 +0200

    Edit Alarm Action designed

---
+++ app/src/main/java/alf/stream/clockradio/EditAlarmActivity.java
@@ -0,0 +1,13 @@
+package alf.stream.clockradio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_alarm);
+    }
+}
